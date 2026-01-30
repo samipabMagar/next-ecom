94b4bec8c6025eb5c9d173bc572e0f53a1696b27
samipabMagar <samipab98@gmail.com>
2026-01-30 13:47:21 +0545

    feat: add loading component and enhance product page with data fetching

---
+++ app/products/[id]/loading.js
@@ -0,0 +1,10 @@
+import Spinner from '@/components/Spinner'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex items-center justify-center'><Spinner/></div>
+  )
+}
+
+export default loading
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import { config } from "@/config/config";
+import axios from "axios";
+import React from "react";
 
-export const page = async({params}) => {
-    const {id} = await params;
+const fetchById = async (id) => {
+  try {
+    const response = await axios.get(`${config.apiUrl}/api/products/${id}`);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching product by ID:", error);
+  }
+};
+
+export const generateMetadata = async ({ params }) => {
+  const { id } = await params;
+
+  const product = await fetchById(id);
+
+  return {
+    title: product?.name || "Product Detail",
+  };
+};
+export const page = async ({ params, searchParams }) => {
+  const { id } = await params;
+  const query = await searchParams;
+
+  const product = await fetchById(id);
   return (
-    <div>Product of id {id}</div>
-  )
-}
+    <div>
+      <h1>Product of id {id}</h1>
+      <h2>{product.brand}</h2>
+      <h2>{product.category}</h2>
+      <h2>{product.price}</h2>
+    </div>
+  );
+};
 
 export default page;
